0e9b32ee730babfdaea72bd095ea9455687c250e
suryaramesh555 <suryaram5566@gmail.com>
2023-10-27 09:32:35 +0530

    index,style,app files added

---
+++ app.js
@@ -0,0 +1,16 @@
+document.getElementById("stuName").innerHTML="Surya R"
+document.getElementById("rolename").innerHTML="Full Stack Developer"
+document.getElementById("aboutmePara").innerText="Aspiring Full Stack Development exhibiting strong foundation with skill of programming to deliver an immersive and engaging user expirience and efficient applications"
+document.getElementById("aboutmePara").style="line-height:25px"
+document.getElementById("qualification1").innerHTML="<tr><td>1</td><td>Msc Computer Science</td><td>2023</td></tr>"
+document.getElementById("qualification2").innerHTML="<tr><td>2</td><td>Bsc Computer Science</td><td>2021</td></tr>"
+document.getElementById("qualification3").innerHTML="<tr><td>3</td><td>HSC</td><td>2018</td></tr>"
+document.getElementById("qualification4").innerHTML="<tr><td>4</td><td>SSLC</td><td>2016</td></tr>"
+document.getElementById("certificationpara").innerHTML="<li>Internship program at ITET for Python Certification</li><li> KGiSL Micro College for Full Stack Development Certification</li>"
+document.getElementById("certificationpara").style="line-height:25px"
+document.getElementById("internshipPara").innerHTML="Full Stack Developer <br>September - Present<br>KGiSL Micro College. Coimbatore"
+document.getElementById("internshipPara").style="line-height:25px"
+document.getElementById("internship").className="internshipClass"
+console.log("Hi This is my resume");
+window.alert("Welcome to my RESUME!");
+window.prompt("Please enter your name to login to see my resume")
